ef82931246fa7c4ad8a6feb76942bcf093592661
Timothy Tampöld <timpakung@hotmail.com>
2021-08-06 20:38:29 +0200

    Make mongoose requests into seperate file

---
+++ models/person.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+var uniqueValidator = require('mongoose-unique-validator')
+
+const url = process.env.MONGODB_URI
+
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(result => {
+    console.log('Connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minLength: 3,
+    unique: true,
+    uniqueCaseInsensitive: true,
+  },
+  number: {
+    type: String,
+    required: true,
+    minLength: 8,
+  },
+})
+
+personSchema.plugin(uniqueValidator)
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
+})
+
+module.exports = mongoose.model('Person', personSchema)
